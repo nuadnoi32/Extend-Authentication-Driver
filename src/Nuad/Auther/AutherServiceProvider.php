ebb18a3187a0e7bbc9c1444a17e08387386280d9
iNuad <nontawat@attendee.co.th>
2013-06-13 15:38:37 +0700

    Update file

--- src/Nuad/Auther/AutherServiceProvider.php
+++ src/Nuad/Auther/AutherServiceProvider.php
@@ -2,48 +2,65 @@
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Auth\Guard;
+use Illuminate\Hashing\HasherInterface;
 
 class AutherServiceProvider extends ServiceProvider {
-
+	
 	/**
 	 * Indicates if loading of the provider is deferred.
 	 *
 	 * @var bool
 	 */
+	
 	protected $defer = false;
-
+	
 	/**
 	 * Bootstrap the application events.
 	 *
 	 * @return void
 	 */
+	
 	public function boot()
 	{
 		$this->package('nuad/auther');
+		
+		\Auth::extend('auther', function() {
+			return $this->createAutherDriver();
+		});
 	}
-
+	
 	/**
 	 * Register the service provider.
 	 *
 	 * @return void
 	 */
+	
 	public function register()
 	{
-		$model = $this->app['config']['auth.model'];
-		$provider = new AutherUserProvider($this->app['hash'], $model);
-		Auth::extend('auther', function() {
-			return new Guard($provider, $this->app['session']);
-		});
+		
+		
 	}
-
+	
 	/**
 	 * Get the services provided by the provider.
 	 *
 	 * @return array
 	 */
+	
 	public function provides()
 	{
-		return array();
+		return array('auther');
 	}
-
+	
+	public function createAutherDriver()
+	{
+		$provider = $this->createAutherProvider();
+		return new Guard($provider, $this->app['session']);
+	}
+	
+	public function createAutherProvider()
+	{
+		$model = $this->app['config']['auth.model'];
+		return new AutherUserProvider($this->app['hash'], $model);
+	}
 }
